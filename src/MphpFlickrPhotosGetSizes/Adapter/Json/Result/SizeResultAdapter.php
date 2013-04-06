84507d950c97b017e70749a17098626df7467496
David White <git@monkeyphp.com>
2013-04-06 12:16:39 +0100

    Added SizeResultAdapter.php

---
+++ src/MphpFlickrPhotosGetSizes/Adapter/Json/Result/SizeResultAdapter.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * SizeResultAdapter.php
+ *
+ * PHP Version PHP 5.3.10
+ *
+ * @category    MphpFlickrPhotosGetSizes
+ * @package     MphpFlickrPhotosGetSizes
+ * @subpackage  MphpFlickrPhotosGetSizes\Adapter\Json\Result
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrPhotosGetSizes\Adapter\Json\Result;
+
+/**
+ * SizeResultAdapter
+ *
+ * @category    MphpFlickrPhotosGetSizes
+ * @package     MphpFlickrPhotosGetSizes
+ * @subpackage  MphpFlickrPhotosGetSizes\Adapter\Json\Result
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+class SizeResultAdapter extends \MphpFlickrBase\Adapter\Json\Result\AbstractResultAdapter implements \MphpFlickrPhotosGetSizes\Adapter\Interfaces\Result\SizeResultAdapterInterface
+{
+    public function getHeight()
+    {
+
+    }
+
+    public function getLabel()
+    {
+
+    }
+
+    public function getMedia()
+    {
+
+    }
+
+    public function getSource()
+    {
+
+    }
+
+    public function getUrl()
+    {
+
+    }
+
+    public function getWidth()
+    {
+
+    }
+    
+}
